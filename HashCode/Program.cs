1b706230a2b0f3ae7081d8ac5a7825d1c2e4504b
msciborski <michal.sciborski@student.put.poznan.pl>
2018-03-01 20:08:28 +0100

    Nie zrobione wczytywanie ale pushuje co mam

--- HashCode/Program.cs
+++ HashCode/Program.cs
@@ -7,17 +7,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HashCode {
-    class Program {
+    public class Program {
         static void Main(string[] args) {
             var roadMap = CreateMap(5, 6);
 
-            //for (var index0 = 0; index0 < roadMap.GetLength(0); index0++) {
-            //    for (var index1 = 0; index1 < roadMap.GetLength(1); index1++) {
-            //        var corner = roadMap[index0, index1];
-            //        Console.Write($"[{corner.Row},{corner.Column}] ");
-            //    }
-            //    Console.WriteLine();
-            //}
             ReadFromFile();
             Console.ReadKey();
         }
@@ -31,12 +24,41 @@ namespace HashCode {
             return roadMap;
         }
         private static void CreateVehicles() {
+            FirstLine firstLine = null;
+            List<Ride> rides = new List<Ride>();
             List<string> linesFromFile = ReadFromFile().AsEnumerable().ToList();
+
             int counter = 0;
             while (linesFromFile.Count != 0) {
                 string[] firstLineSplited = linesFromFile.First().Split(' ');
+                linesFromFile.RemoveAt(0);
+
+                counter++;
+
+                firstLine = ProcessFirstLine(firstLineSplited);
+
+                for (int i = 0; i < firstLine.Rides; i++) {
+                    string[] ride = linesFromFile.First().Split(' ');
+                }
             }
 
+        }
+        private static Ride ProcessRide(string[] ride) {
+            for(int i = 0; i < ride.Length; i++) {
+                if(i+2 == ride.Length) {
+                    
+                }
+            }
+        }
+        private static FirstLine ProcessFirstLine(string[] firstLine) {
+            return new FirstLine() {
+                Rows= Int32.Parse(firstLine[0]),
+                Columns = Int32.Parse(firstLine[1]),
+                Vehicles = Int32.Parse(firstLine[2]),
+                Rides = Int32.Parse(firstLine[3]),
+                Bonus = Int32.Parse(firstLine[4]),
+                Steps = Int32.Parse(firstLine[5])
+            };
         }
         private static string[] ReadFromFile() {
             string[] lines = File.ReadAllLines("C:\\Users\\mscib\\source\\repos\\HashCode\\a_example.in");
